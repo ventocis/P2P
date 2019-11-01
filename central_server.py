be64a88394575146b82614b1a8898511c4c56fb2
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2019-11-01 10:53:20 -0400

    Skeleton code for the server

--- central_server.py
+++ central_server.py
@@ -1,3 +1,37 @@
 import socket
 import threading
 import os
+
+
+IP = '127.0.0.1'
+PORT = 12000
+
+class Client(threading.Thread):
+    def __init__(self, s, addr):
+        self.request = s
+        self.addr = addr
+        super(Client, self).__init__()
+
+    def run(self):
+        while(True):
+            #wait for client to send username, hostname, and connection speed
+            #store username, hostname, and connection speed in a table/list
+            #send acknowledgement back to client
+            #wait for client to send xml file with shared file descriptions
+            #parse xml file and store descriptions in a table/list
+            #wait for user to send a keyword search
+
+        
+    def quit(self):
+        print("Client Has Disconnected")
+        self.request.close()    
+
+serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serv.bind((IP, PORT))
+serv.listen(1)
+
+while True:
+    conn, addr = serv.accept()
+    print("USER: " + str(addr) + " CONNECTED")
+    client_thr = Client(conn, addr)
+    client_thr.start()
