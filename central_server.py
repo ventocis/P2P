b4b4e3e6d1c27e96983d6e043faa02fbdfc6f0a8
Alistair The Gecko <ihallwatt@gmail.com>
2019-11-06 10:44:58 -0500

    User and File dictionary storage methods

--- central_server.py
+++ central_server.py
@@ -5,6 +5,8 @@ import os
 
 IP = '127.0.0.1'
 PORT = 12000
+userDict = {}
+fileDict = {}
 
 class Client(threading.Thread):
     def __init__(self, s, addr):
@@ -14,6 +16,8 @@ class Client(threading.Thread):
 
     def run(self):
         while(True):
+
+
             #wait for client to send username, hostname, and connection speed
             #store username, hostname, and connection speed in a table/list
             #send acknowledgement back to client
@@ -24,7 +28,20 @@ class Client(threading.Thread):
         
     def quit(self):
         print("Client Has Disconnected")
-        self.request.close()    
+        self.request.close()
+
+    def storeUsers(self, username, hostName, portNumber, connSpeed):
+        userInfo = [hostName, portNumber, connSpeed]
+        userDict[username] = userInfo
+
+    def storeFiles(self, username, fileName, description):
+        fileInfo = [fileName, description]
+        fileDict[username] = fileInfo
+
+    def deleteUser(self, username):
+        del fileDict[username]
+        del userDict[username]
+
 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind((IP, PORT))
