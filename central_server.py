862db3a56adec221761165cf9851d73822093826
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2019-11-06 10:45:24 -0500

    Parse XML File

--- central_server.py
+++ central_server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import os
+import xml.etree.ElementTree as ET
 
 
 IP = '127.0.0.1'
@@ -21,7 +22,33 @@ class Client(threading.Thread):
             #parse xml file and store descriptions in a table/list
             #wait for user to send a keyword search
 
-        
+    def stor(self, s, fileName):
+            myString = "STOR "
+            myString = myString + fileName
+            s.send(myString.encode('utf-8'))
+            f = open(fileName, "w")
+            print("Created file " + fileName)
+            line = s.recv(1024).decode('utf-8')
+            while line:
+                f.write(line)
+                line = s.recv(1024).decode('utf-8')
+            f.close()
+            print("File Downloaded")
+
+    def parseXML(self, fileName, userName):
+        if path.exists(fileName):
+            tree = ET.parse(fileName)
+            root = tree.getroot()
+            for child in root:
+                name = child[0]
+                descr = child[1]
+                #call function to store files
+            print("Successfully parsed XML file")
+        else:
+            print("Parse Failed")
+            s.send("Parse Failed".encode('utf-8'))
+        os.remove(fileName)
+
     def quit(self):
         print("Client Has Disconnected")
         self.request.close()    
