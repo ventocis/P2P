f7905b2bb64a232d62b88975bd3972ea2d6d9263
wolverine18 <37334449+wolverine18@users.noreply.github.com>
2019-11-08 12:03:57 -0500

    Connection commands

--- central_server.py
+++ central_server.py
@@ -2,6 +2,7 @@ import socket
 import threading
 import os
 import xml.etree.ElementTree as ET
+from os import listdir, path
 
 
 IP = '127.0.0.1'
@@ -13,18 +14,46 @@ class Client(threading.Thread):
     def __init__(self, s, addr):
         self.request = s
         self.addr = addr
+        self.port = addr[1]
         super(Client, self).__init__()
 
     def run(self):
         while(True):
-
-
+            command = self.request.recv(1024).decode('utf-8').split()
+            if command[0] == "CONNECT":
+                self.connect(command)
+                return
+                    
+            #CONNECT Username Hostname ConnectionSpeed
+            #Send ACK command name
+            #FILEDESC FileName Username
+            #SEARCH DESCR Username
+            #LOCATION XMLFile
+            #QUIT Username
             #wait for client to send username, hostname, and connection speed
             #store username, hostname, and connection speed in a table/list
             #send acknowledgement back to client
             #wait for client to send xml file with shared file descriptions
             #parse xml file and store descriptions in a table/list
             #wait for user to send a keyword search
+    def connect(self, command):
+        try:
+            s = socket.socekt(socket.AF_INET, socket.SOCK_STREAM)
+            self.storeUsers(self.port, command[1], command[2], command[3])
+            s.connect((IP, self.port))
+            s.send("ACK CONNECT".encode('utf-8'))
+            while(True):
+                if command[0] == "QUIT":
+                    self.quit(command[1])
+                    return
+                elif command[0] == "FILEDESC":
+                    s.send("ACK FILEDESC".encode('utf-8'))
+                    self.stor(s, command[1])
+                    self.parseXML(s, command[1], command[2])
+                elif command[0] == "SEARCH":
+                    self.search(command[1], command[2])
+        except socket.error as exc:
+            print("Connection error: " + str(exc))
 
     def stor(self, s, fileName):
             myString = "STOR "
@@ -39,21 +68,23 @@ class Client(threading.Thread):
             f.close()
             print("File Downloaded")
 
-    def parseXML(self, fileName, userName):
+    def parseXML(self, s, fileName, userName):
         if path.exists(fileName):
             tree = ET.parse(fileName)
             root = tree.getroot()
             for child in root:
-                name = child[0]
-                descr = child[1]
-                #call function to store files
+                name = child[0].text
+                descr = child[1].text
+                self.storeFiles(userName, name, descr)
             print("Successfully parsed XML file")
+            print(fileDict)
+            os.remove(fileName)
         else:
             print("Parse Failed")
             s.send("Parse Failed".encode('utf-8'))
-        os.remove(fileName)
 
-    def quit(self):
+    def quit(self, userName):
+        #need to delete username and files
         print("Client Has Disconnected")
         self.request.close()
 
@@ -64,12 +95,13 @@ class Client(threading.Thread):
     def storeFiles(self, username, fileName, description):
         fileInfo = [fileName, description]
         fileDict[username] = fileInfo
-
+        
     def deleteUser(self, username):
         del fileDict[username]
         del userDict[username]
 
-
+    def search(self, srchString, userName):
+        print("In search")
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind((IP, PORT))
 serv.listen(1)
